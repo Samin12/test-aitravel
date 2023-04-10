b8ffaadc8dc6201fbaacd21c4a1ced7937649497
Samin Yasar <saminyasar@samins-mbp-2.lan>
2023-04-09 23:31:18 -0400

    comment out open db

--- src/routes/api/getMediaDetails/+server.ts
+++ src/routes/api/getMediaDetails/+server.ts
@@ -1,11 +1,10 @@
-import { OMDB_API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }: { request: any }) {
-	const { title } = await request.json();
-	const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`;
+	// const { title } = await request.json();
+	// const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${title}`;
 
-	const res = await fetch(url);
-	const details = await res.json();
-	return json(details);
+	// const res = await fetch(url);
+	// const details = await res.json();
+	return json(request);
 }
